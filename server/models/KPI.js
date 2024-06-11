fb5634d7f934c323a8b90b5a40f2da67203b1fa4
Marikha Mehu <mmehu777@gmail.com>
2024-06-10 21:10:54 -0400

    creating kpi routes and models

---
+++ server/models/KPI.js
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+import { loadType } from "mongoose-currency";
+
+const Schema = mongoose.Schema;
+loadType(mongoose);
+
+const KPISchema = new Schema(
+    {
+        totalProfit: {
+            type: mongoose.Types.Currency,
+            currency: "USD",
+            get: (v) => v / 100
+        },
+        totalRevenue: {
+            type: mongoose.Types.Currency,
+            currency: "USD",
+            get: (v) => v / 100
+        },
+        totalExpenses: {
+            type: mongoose.Types.Currency,
+            currency: "USD",
+            get: (v) => v / 100
+        },
+        expenseByCategory: {
+            type: Map,
+            of: {
+                type: mongoose.Types.Currency,
+                currency: "USD",
+                get: (v) => v / 100
+            }
+                
+        }
+    }
+);
+
+const KPI = mongoose.model("KPI", KPISchema);
+
+export default KPI;
