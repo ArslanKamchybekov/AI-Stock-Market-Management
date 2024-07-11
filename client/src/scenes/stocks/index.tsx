152b7adec5276786e8447ff41b677f74c2539a27
arelymartinez16 <arelymartinez741@gmail.com>
2024-07-10 22:12:22 -0400

    Set up routing for stocks tab

---
+++ client/src/scenes/stocks/index.tsx
@@ -0,0 +1,11 @@
+import DashboardBox from "@/components/DashboardBox"
+
+const Stocks = () => {
+    return (
+        <DashboardBox width="100%" height="100%" p="1rem" overflow="hidden">
+
+        </DashboardBox>
+    )
+}
+
+export default Stocks
